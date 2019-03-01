4ecb0222ea5f4a38ec5ee7097a0e272cdc08c9ef
mayoi7 <3451669860@qq.com>
2019-03-01 10:14:27 +0800

    init(#ALL): init project

---
+++ src/com/ushiro/demo/Application.java
@@ -0,0 +1,12 @@
+package com.ushiro.demo;
+
+/**
+ * @author Kripath
+ * @date Created in 10:11 2019/3/1
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Design Pattern...");
+    }
+}
